1a3bb91ef13e0cd5fbab38fd1e74ebf717a15730
garageagle <garageagle@gmail.com>
2020-04-02 21:06:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TestingGroundsFPS
+
+A Stealth/Brute Force FPS game eveloped with Unreal Engine 4.
---
+++ Source/TestingGroundsFPS.Target.cs
@@ -0,0 +1,14 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TestingGroundsFPSTarget : TargetRules
+{
+	public TestingGroundsFPSTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("TestingGroundsFPS");
+	}
+}
